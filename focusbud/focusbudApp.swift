db591c6a6a8893c41d318570fa11165b14879a54
kocho31 <99873300+kocho31@users.noreply.github.com>
2025-04-05 17:17:02 +0100

    Initial Commit

---
+++ focusbud/focusbudApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusbudApp.swift
+//  focusbud
+//
+//  Created by Alexandre Martins on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct focusbudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
